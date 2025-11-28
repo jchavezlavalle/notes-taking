a45c05ef5a2f8a1a47964004f6385bd8db6ff338
jchavezlavalle <jchavezlavalle@gmail.com>
2025-11-28 01:13:06 -0500

    add sign in page

--- app/config.tsx
+++ app/config.tsx
@@ -4,5 +4,6 @@ export const config = {
     message_when_no_notes: process.env.MESSAGE_WHEN_NO_NOTES || "I'm just here waiting for your charming notes...",
     default_category: Number(process.env.DEFAULT_CATEGORY_SELECTED) || 1, // When creating notes we have a default category
     default_login_welcome_message: process.env.DEFAULT_LOGIN_WELCOME_MESSAGE || "Yay, New Friend!",
+    default_signin_welcome_message: process.env.DEFAULT_SIGNIN_WELCOME_MESSAGE || "Yay, You're Back!",
 }
     
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { config } from "./config";
+import { config } from "../config";
 import { Inria_Serif } from "next/font/google";
 import Link from "next/link";
 
@@ -11,7 +11,7 @@ const inria = Inria_Serif({
   weight: ["400", "700"],
 });
 
-export default function LoginPage() {
+export default function SignInPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -34,11 +34,11 @@ export default function LoginPage() {
         className="flex flex-col gap-4 w-800"
       >
         <img 
-    src="/welcome_img.png" 
-    alt="welcome image"
-    className="w-48 h-auto mx-auto mb-4 welcome-cat"
+    src="/sign_in.png" 
+    alt="welcome back image"
+    className="w-48 h-auto mx-auto mb-4 welcome-back-cactus"
   />
-        <h1 className={`${inria.className} font-bold block text-center mb-2 welcome-text`}>{config.default_login_welcome_message || ""}</h1>
+        <h1 className={`${inria.className} font-bold block text-center mb-2 welcome-text`}>{config.default_signin_welcome_message || ""}</h1>
 
         <input
           className="border p-2 rounded placeholder-black w-[380px]"
@@ -63,10 +63,10 @@ export default function LoginPage() {
           style={{borderWidth:"1px", borderColor: "#957139"}}
           className="p-2 rounded border-elements border rounded-l-full rounded-r-full font-bold w-[380px]"
         >
-          Sign Up
+          Login
         </button>
-        <Link className="block text-center underline text-[12px] mt-[-10px] cursor-pointer" style={{color: "#957139"}} href="/signin">
-          We're already friends!
+        <Link className="block text-center underline text-[12px]  mt-[-10px] cursor-pointer" style={{color: "#957139"}} href="/">
+          Oops! I've never been here before
         </Link>
       </form>
     </div>
